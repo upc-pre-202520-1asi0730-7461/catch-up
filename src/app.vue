8c4e92fadea3d8bce79fa7e599a61a4129325651
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-15 11:45:42 -0500

    feat(layout): add main layout.

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,10 @@
 <script setup>
 
+import Layout from "./shared/presentation/components/layout.vue";
 </script>
 
 <template>
-
+  <layout></layout>
 </template>
 
 <style scoped>
---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,37 @@
+<script setup lang="js">
+
+import {ref} from "vue";
+import {Menubar as PvMenubar} from "primevue";
+import LanguageSwitcher from "./language-switcher.vue";
+import FooterContent from "./footer-content.vue";
+
+const drawerVisible = ref(false);
+const toggleDrawer = () => {
+  drawerVisible.value = !drawerVisible.value;
+};
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <pv-menubar>
+        <template #start>
+          <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleDrawer"/>
+          <!-- TODO: Source List -->
+        </template>
+        <template #end>
+          <language-switcher></language-switcher>
+        </template>
+      </pv-menubar>
+    </div>
+  </div>
+  <div>
+    <!-- TODO: Article List -->
+  </div>
+  <footer-content></footer-content>
+</template>
+
+<style scoped>
+
+</style>
